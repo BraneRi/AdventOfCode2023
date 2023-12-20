2d6a509e26cb17e729f96574c1c970cadc4224d3
branimir <branimir.akmadza88@gmail.com>
2023-12-20 22:39:31 +0100

    Implement idea with expanded matrix with consecutive steps. Needs debuggingg.

---
+++ day17/day17.js
@@ -0,0 +1,298 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline");
+var fs = require("fs");
+var PriorityQueue = /** @class */ (function () {
+    function PriorityQueue() {
+        this.items = [];
+    }
+    PriorityQueue.prototype.enqueue = function (key, priority) {
+        this.items.push({ key: key, priority: priority });
+        this.sort();
+    };
+    PriorityQueue.prototype.dequeue = function () {
+        var _a;
+        return (_a = this.items.shift()) === null || _a === void 0 ? void 0 : _a.key;
+    };
+    PriorityQueue.prototype.sort = function () {
+        this.items.sort(function (a, b) { return a.priority - b.priority; });
+    };
+    PriorityQueue.prototype.isEmpty = function () {
+        return this.items.length === 0;
+    };
+    return PriorityQueue;
+}());
+function processFile(filePath) {
+    var _a, e_1, _b, _c;
+    return __awaiter(this, void 0, void 0, function () {
+        var fileStream, rl, input, _d, rl_1, rl_1_1, line, e_1_1, graph, startKey;
+        return __generator(this, function (_e) {
+            switch (_e.label) {
+                case 0:
+                    fileStream = fs.createReadStream(filePath);
+                    rl = readline.createInterface({
+                        input: fileStream,
+                        crlfDelay: Infinity, // Treats each line as a separate data event
+                    });
+                    input = [];
+                    _e.label = 1;
+                case 1:
+                    _e.trys.push([1, 6, 7, 12]);
+                    _d = true, rl_1 = __asyncValues(rl);
+                    _e.label = 2;
+                case 2: return [4 /*yield*/, rl_1.next()];
+                case 3:
+                    if (!(rl_1_1 = _e.sent(), _a = rl_1_1.done, !_a)) return [3 /*break*/, 5];
+                    _c = rl_1_1.value;
+                    _d = false;
+                    line = _c;
+                    input.push(line.split("").map(function (char) { return Number(char); }));
+                    _e.label = 4;
+                case 4:
+                    _d = true;
+                    return [3 /*break*/, 2];
+                case 5: return [3 /*break*/, 12];
+                case 6:
+                    e_1_1 = _e.sent();
+                    e_1 = { error: e_1_1 };
+                    return [3 /*break*/, 12];
+                case 7:
+                    _e.trys.push([7, , 10, 11]);
+                    if (!(!_d && !_a && (_b = rl_1.return))) return [3 /*break*/, 9];
+                    return [4 /*yield*/, _b.call(rl_1)];
+                case 8:
+                    _e.sent();
+                    _e.label = 9;
+                case 9: return [3 /*break*/, 11];
+                case 10:
+                    if (e_1) throw e_1.error;
+                    return [7 /*endfinally*/];
+                case 11: return [7 /*endfinally*/];
+                case 12:
+                    graph = generateGraph(input);
+                    //TODO  analyse graph
+                    console.log(graph);
+                    startKey = {
+                        row: 0,
+                        col: 0,
+                        consecutiveLeft: 0,
+                        consecutiveRight: 0,
+                        consecutiveUp: 0,
+                        consecutiveDown: 0,
+                    };
+                    console.log(dijkstra(graph, startKey, input.length - 1, input[0].length - 1, input));
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function toNodeKeyFromString(stringKey) {
+    var keyParts = stringKey.split(",");
+    return {
+        row: Number(keyParts[0]),
+        col: Number(keyParts[1]),
+        consecutiveLeft: Number(keyParts[2]),
+        consecutiveRight: Number(keyParts[3]),
+        consecutiveUp: Number(keyParts[4]),
+        consecutiveDown: Number(keyParts[5]),
+    };
+}
+function generateGraph(originalGraph) {
+    var _a;
+    var newGraph = new Map();
+    // Create nodes in the new graph based on the rule
+    for (var row = 0; row < originalGraph.length; row++) {
+        for (var col = 0; col < originalGraph[0].length; col++) {
+            var nodeKey = {
+                row: row,
+                col: col,
+                consecutiveLeft: 0,
+                consecutiveRight: 0,
+                consecutiveUp: 0,
+                consecutiveDown: 0,
+            };
+            newGraph.set(toStringKey(nodeKey), []);
+            //todo remove out of bounds ones
+            for (var i = 1; i <= 3; i++) {
+                newGraph.set(toStringKey({
+                    row: row,
+                    col: col,
+                    consecutiveLeft: i,
+                    consecutiveRight: 0,
+                    consecutiveUp: 0,
+                    consecutiveDown: 0,
+                }), []);
+                newGraph.set(toStringKey({
+                    row: row,
+                    col: col,
+                    consecutiveLeft: 0,
+                    consecutiveRight: i,
+                    consecutiveUp: 0,
+                    consecutiveDown: 0,
+                }), []);
+                newGraph.set(toStringKey({
+                    row: row,
+                    col: col,
+                    consecutiveLeft: 0,
+                    consecutiveRight: 0,
+                    consecutiveUp: i,
+                    consecutiveDown: 0,
+                }), []);
+                newGraph.set(toStringKey({
+                    row: row,
+                    col: col,
+                    consecutiveLeft: 0,
+                    consecutiveRight: 0,
+                    consecutiveUp: 0,
+                    consecutiveDown: i,
+                }), []);
+            }
+        }
+    }
+    for (var _i = 0, _b = Array.from(newGraph.entries()); _i < _b.length; _i++) {
+        var _c = _b[_i], currentNodeKey = _c[0], _ = _c[1];
+        var _d = toNodeKeyFromString(currentNodeKey), row = _d.row, col = _d.col, consecutiveLeft = _d.consecutiveLeft, consecutiveRight = _d.consecutiveRight, consecutiveUp = _d.consecutiveUp, consecutiveDown = _d.consecutiveDown;
+        var deltas = [];
+        // If we went right, we cannot go back left
+        if (consecutiveRight == 0) {
+            for (var i = consecutiveLeft; i <= 3; i++) {
+                deltas.push([-1 * i, 0, i, 0, 0, 0]);
+            }
+        }
+        if (consecutiveLeft == 0) {
+            for (var i = consecutiveRight; i <= 3; i++) {
+                deltas.push([i, 0, 0, i, 0, 0]);
+            }
+        }
+        if (consecutiveDown == 0) {
+            for (var i = consecutiveUp; i <= 3; i++) {
+                deltas.push([0, -1 * i, 0, 0, i, 0]);
+            }
+        }
+        if (consecutiveUp == 0) {
+            for (var i = consecutiveDown; i <= 3; i++) {
+                deltas.push([0, i, 0, 0, 0, i]);
+            }
+        }
+        for (var _e = 0, deltas_1 = deltas; _e < deltas_1.length; _e++) {
+            var _f = deltas_1[_e], deltaRow = _f[0], deltaCol = _f[1], left = _f[2], right = _f[3], up = _f[4], down = _f[5];
+            var newRow = row + deltaRow;
+            var newCol = col + deltaCol;
+            if (newRow >= 0 &&
+                newRow < originalGraph.length &&
+                newCol >= 0 &&
+                newCol < originalGraph[0].length) {
+                var newNodeKey = {
+                    row: newRow,
+                    col: newCol,
+                    consecutiveLeft: left,
+                    consecutiveRight: right,
+                    consecutiveUp: up,
+                    consecutiveDown: down,
+                };
+                (_a = newGraph.get(currentNodeKey)) === null || _a === void 0 ? void 0 : _a.push(newNodeKey);
+            }
+        }
+    }
+    return newGraph;
+}
+function toStringKey(key) {
+    return (key.row +
+        "," +
+        key.col +
+        "," +
+        key.consecutiveLeft +
+        "," +
+        key.consecutiveRight +
+        "," +
+        key.consecutiveUp +
+        "," +
+        key.consecutiveDown);
+}
+function dijkstra(graph, startKey, targetRow, targetColumn, input) {
+    console.log(graph);
+    var distances = {};
+    var priorityQueue = new PriorityQueue();
+    distances[toStringKey(startKey)] = 0;
+    priorityQueue.enqueue(startKey, 0);
+    while (!priorityQueue.isEmpty()) {
+        var currentNodeKey = priorityQueue.dequeue();
+        if (!currentNodeKey) {
+            break;
+        }
+        for (var _i = 0, _a = graph.get(toStringKey(currentNodeKey)); _i < _a.length; _i++) {
+            var neighborKey = _a[_i];
+            var distanceToNeighbor = distances[toStringKey(currentNodeKey)] +
+                input[neighborKey.row][neighborKey.col];
+            if (distances[toStringKey(neighborKey)] == undefined ||
+                distanceToNeighbor < distances[toStringKey(neighborKey)]) {
+                distances[toStringKey(neighborKey)] = distanceToNeighbor;
+                priorityQueue.enqueue(neighborKey, distanceToNeighbor);
+            }
+        }
+    }
+    var shortestTarget = Number.MAX_SAFE_INTEGER;
+    var shortest = Object.entries(distances)
+        .filter(function (_a) {
+        var key = _a[0], _ = _a[1];
+        var nodeKey = toNodeKeyFromString(key);
+        return nodeKey.row == targetRow && nodeKey.col == targetColumn;
+    })
+        .map(function (targets) { return targets[1]; })
+        .reduce(function (acc, entry) { return Math.min(acc, entry); }, shortestTarget);
+    // console.log(distances);
+    return shortest || -1; // Return -1 if target is unreachable
+}
+// Usage: node build/your-script.js your-text-file.txt
+var args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.error("Usage: node build/your-script.js your-text-file.txt");
+    process.exit(1);
+}
+var filePath = args[0];
+processFile(filePath)
+    .then(function () { return console.log("File processing completed."); })
+    .catch(function (error) { return console.error("Error:", error); });
---
+++ day17/day17.ts
@@ -0,0 +1,291 @@
+import * as readline from "readline";
+import * as fs from "fs";
+
+type NodeKey = {
+  row: number;
+  col: number;
+  consecutiveLeft: number;
+  consecutiveRight: number;
+  consecutiveUp: number;
+  consecutiveDown: number;
+};
+type Graph = Map<string, NodeKey[]>;
+
+class PriorityQueue {
+  items: { key: NodeKey; priority: number }[];
+
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(key: NodeKey, priority: number) {
+    this.items.push({ key, priority });
+    this.sort();
+  }
+
+  dequeue(): NodeKey | undefined {
+    return this.items.shift()?.key;
+  }
+
+  sort() {
+    this.items.sort((a, b) => a.priority - b.priority);
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+}
+
+async function processFile(filePath: string): Promise<void> {
+  // Create a readable stream from the file
+  const fileStream = fs.createReadStream(filePath);
+
+  // Create an interface to read lines from the stream
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity, // Treats each line as a separate data event
+  });
+
+  const input: number[][] = [];
+  for await (const line of rl) {
+    input.push(line.split("").map((char) => Number(char)));
+  }
+
+  const graph = generateGraph(input);
+
+  //TODO  analyse graph
+  console.log(graph);
+
+  const startKey = {
+    row: 0,
+    col: 0,
+    consecutiveLeft: 0,
+    consecutiveRight: 0,
+    consecutiveUp: 0,
+    consecutiveDown: 0,
+  };
+  console.log(
+    dijkstra(graph, startKey, input.length - 1, input[0].length - 1, input)
+  );
+}
+
+function toNodeKeyFromString(stringKey: string): NodeKey {
+  const keyParts = stringKey.split(",");
+  return {
+    row: Number(keyParts[0]),
+    col: Number(keyParts[1]),
+    consecutiveLeft: Number(keyParts[2]),
+    consecutiveRight: Number(keyParts[3]),
+    consecutiveUp: Number(keyParts[4]),
+    consecutiveDown: Number(keyParts[5]),
+  };
+}
+
+function generateGraph(originalGraph: number[][]): Graph {
+  const newGraph: Graph = new Map();
+
+  // Create nodes in the new graph based on the rule
+  for (let row = 0; row < originalGraph.length; row++) {
+    for (let col = 0; col < originalGraph[0].length; col++) {
+      var nodeKey = {
+        row: row,
+        col: col,
+        consecutiveLeft: 0,
+        consecutiveRight: 0,
+        consecutiveUp: 0,
+        consecutiveDown: 0,
+      };
+      newGraph.set(toStringKey(nodeKey), []);
+      //todo remove out of bounds ones
+      for (let i = 1; i <= 3; i++) {
+        newGraph.set(
+          toStringKey({
+            row: row,
+            col: col,
+            consecutiveLeft: i,
+            consecutiveRight: 0,
+            consecutiveUp: 0,
+            consecutiveDown: 0,
+          }),
+          []
+        );
+        newGraph.set(
+          toStringKey({
+            row: row,
+            col: col,
+            consecutiveLeft: 0,
+            consecutiveRight: i,
+            consecutiveUp: 0,
+            consecutiveDown: 0,
+          }),
+          []
+        );
+        newGraph.set(
+          toStringKey({
+            row: row,
+            col: col,
+            consecutiveLeft: 0,
+            consecutiveRight: 0,
+            consecutiveUp: i,
+            consecutiveDown: 0,
+          }),
+          []
+        );
+        newGraph.set(
+          toStringKey({
+            row: row,
+            col: col,
+            consecutiveLeft: 0,
+            consecutiveRight: 0,
+            consecutiveUp: 0,
+            consecutiveDown: i,
+          }),
+          []
+        );
+      }
+    }
+  }
+
+  for (const [currentNodeKey, _] of Array.from(newGraph.entries())) {
+    const {
+      row,
+      col,
+      consecutiveLeft,
+      consecutiveRight,
+      consecutiveUp,
+      consecutiveDown,
+    } = toNodeKeyFromString(currentNodeKey);
+
+    const deltas: number[][] = [];
+
+    // If we went right, we cannot go back left
+    if (consecutiveRight == 0) {
+      for (let i = consecutiveLeft; i <= 3; i++) {
+        deltas.push([-1 * i, 0, i, 0, 0, 0]);
+      }
+    }
+
+    if (consecutiveLeft == 0) {
+      for (let i = consecutiveRight; i <= 3; i++) {
+        deltas.push([i, 0, 0, i, 0, 0]);
+      }
+    }
+
+    if (consecutiveDown == 0) {
+      for (let i = consecutiveUp; i <= 3; i++) {
+        deltas.push([0, -1 * i, 0, 0, i, 0]);
+      }
+    }
+
+    if (consecutiveUp == 0) {
+      for (let i = consecutiveDown; i <= 3; i++) {
+        deltas.push([0, i, 0, 0, 0, i]);
+      }
+    }
+
+    for (const [deltaRow, deltaCol, left, right, up, down] of deltas) {
+      const newRow = row + deltaRow;
+      const newCol = col + deltaCol;
+
+      if (
+        newRow >= 0 &&
+        newRow < originalGraph.length &&
+        newCol >= 0 &&
+        newCol < originalGraph[0].length
+      ) {
+        const newNodeKey: NodeKey = {
+          row: newRow,
+          col: newCol,
+          consecutiveLeft: left,
+          consecutiveRight: right,
+          consecutiveUp: up,
+          consecutiveDown: down,
+        };
+        newGraph.get(currentNodeKey)?.push(newNodeKey);
+      }
+    }
+  }
+
+  return newGraph;
+}
+
+function toStringKey(key: NodeKey) {
+  return (
+    key.row +
+    "," +
+    key.col +
+    "," +
+    key.consecutiveLeft +
+    "," +
+    key.consecutiveRight +
+    "," +
+    key.consecutiveUp +
+    "," +
+    key.consecutiveDown
+  );
+}
+
+function dijkstra(
+  graph: Map<string, NodeKey[]>,
+  startKey: NodeKey,
+  targetRow: number,
+  targetColumn: number,
+  input: number[][]
+): number {
+  console.log(graph);
+
+  const distances: Record<string, number> = {};
+  const priorityQueue = new PriorityQueue();
+
+  distances[toStringKey(startKey)] = 0;
+  priorityQueue.enqueue(startKey, 0);
+
+  while (!priorityQueue.isEmpty()) {
+    const currentNodeKey = priorityQueue.dequeue();
+
+    if (!currentNodeKey) {
+      break;
+    }
+
+    for (const neighborKey of graph.get(toStringKey(currentNodeKey))!) {
+      const distanceToNeighbor =
+        distances[toStringKey(currentNodeKey)] +
+        input[neighborKey.row][neighborKey.col];
+
+      if (
+        distances[toStringKey(neighborKey)] == undefined ||
+        distanceToNeighbor < distances[toStringKey(neighborKey)]
+      ) {
+        distances[toStringKey(neighborKey)] = distanceToNeighbor;
+        priorityQueue.enqueue(neighborKey, distanceToNeighbor);
+      }
+    }
+  }
+
+  var shortestTarget = Number.MAX_SAFE_INTEGER;
+
+  const shortest = Object.entries(distances)
+    .filter(([key, _]) => {
+      const nodeKey = toNodeKeyFromString(key);
+      return nodeKey.row == targetRow && nodeKey.col == targetColumn;
+    })
+    .map((targets) => targets[1])
+    .reduce((acc, entry) => Math.min(acc, entry), shortestTarget);
+
+  // console.log(distances);
+  return shortest || -1; // Return -1 if target is unreachable
+}
+
+// Usage: node build/your-script.js your-text-file.txt
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  console.error("Usage: node build/your-script.js your-text-file.txt");
+  process.exit(1);
+}
+
+const filePath = args[0];
+
+processFile(filePath)
+  .then(() => console.log("File processing completed."))
+  .catch((error) => console.error("Error:", error));
